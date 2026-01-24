37bbcee140e3e8bb4b8f4c0c0fb830b5caee949a
alejandrotellez539130-web <alejandrotellez539130@gmail.com>
2026-01-23 20:41:54 -0600

    Crear dante_dna.js

---
+++ js/dante_dna.js
@@ -0,0 +1,10 @@
+const ODU_DB = {
+"Ogbe": {b:"1111", v:"Claridad", d:"Ego"}, "Yekun": {b:"0000", v:"Profundidad", d:"Muerte"},
+"Iwori": {b:"0110", v:"Lógica", d:"Duda"}, "Odi": {b:"1001", v:"Libertad", d:"Bloqueo"},
+"Irosun": {b:"0101", v:"Visión", d:"Ilusión"}, "Owonrin": {b:"1010", v:"Cambio", d:"Caos"},
+"Obara": {b:"0111", v:"Voz", d:"Mentira"}, "Okanran": {b:"1000", v:"Justicia", d:"Ira"},
+"Ogunda": {b:"1110", v:"Fuerza", d:"Violencia"}, "Osa": {b:"0100", v:"Viento", d:"Miedo"},
+"Ika": {b:"1100", v:"Poder", d:"Maldad"}, "Oturupon": {b:"0011", v:"Salud", d:"Enfermedad"},
+"Otura": {b:"1011", v:"Verdad", d:"Confusión"}, "Irete": {b:"0010", v:"Presión", d:"Crueldad"},
+"Ose": {b:"1101", v:"Amor", d:"Celos"}, "Ofun": {b:"0001", v:"Milagro", d:"Avaricia"}
+};
