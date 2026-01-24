5ee0fc2c095896b6a1431f3b932516a5cafdd72e
alejandrotellez539130-web <alejandrotellez539130@gmail.com>
2026-01-23 21:14:57 -0600

    Actualizar dante_engine.js

--- js/dante_engine.js
+++ js/dante_engine.js
@@ -1,1 +1,9 @@
 
+function calcularOduMaestro(fecha) {
+    const f = fecha.toString();
+    const claves = Object.keys(ODU_DB);
+    // Algoritmo de extracción de 4 bits basado en la fecha
+    const indexP1 = parseInt(f.slice(-2)) % 16;
+    const indexP2 = parseInt(f.slice(4,6)) % 16;
+    return { padre: claves[indexP1], madre: claves[indexP2] };
+}
